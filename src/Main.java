1b9edca6e89c7501e8adc3ba202a6bb98760fb3d
Slav1k <maznevslava@yandex.ru>
2022-08-16 23:39:08 +0500

    dz6

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
+    public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int [30];
-        int sum = 0;
+        int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
-            sum += arr[i];
-            System.out.println("Сумма за месяц" + sum + " рублей");
         }
-        int maxMany = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >maxMany) {
-                maxMany=arr[i];
-                System.out.println("Макс сумма в месяц "+ maxMany);
-            }
-        }
-        int minMany= 1;
-        for (int i = 0; i < arr.length; i--) {
-            if (arr[i]> minMany){
-                minMany=arr[i];
-                System.out.println("Мин "+ minMany);
-            }
+        return arr;
+    }
 
-        }
-        }
+    //Задание 1
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        for (int i = 0; i < arr.length; i++) {
+            int arr1 = 0;
+            arr1 = arr[i];
+                System.out.println("Сумма трат за месяц составила " + arr1 + " рублей ");
             }
+        }
+    }
