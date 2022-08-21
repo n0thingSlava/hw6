ca21806a7c1cc0220a9bd6528ccde5ba285cb86b
Slav1k <maznevslava@yandex.ru>
2022-08-21 22:30:07 +0500

    dz6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
-        System.out.println(arr[i]);
         }
+        System.out.println(Arrays.toString(arr));
         return arr;
     }
     //Задание 1
